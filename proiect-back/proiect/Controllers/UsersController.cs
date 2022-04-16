cce5f24cb2632fb5e3b32b29b053f6fdcacbef33
Eusebiu Popescu <popescusebi1234@yahoo.com>
2022-04-16 11:53:32 +0300

    work

---
+++ proiect-back/proiect/Controllers/UsersController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using proiect.modells;
+
+namespace proiect.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public UsersController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                            select UserId, UserName from dbo.Users
+                        ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("UsersAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+        [HttpPost]
+        public JsonResult Post(Users us)
+        {
+            string query = @"
+                           insert into dbo.Users
+                            values (@UserName)
+                        ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("UsersAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@UserName", us.UserName);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Successfully");
+        }
+        [HttpPut]
+        public JsonResult Put(Users us)
+        {
+            string query = @"
+                           update dbo.Users
+                           set UserName = (@UserName)
+                            where UserId=@UserId
+                        ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("UsersAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@UserId", us.UserId);
+                    myCommand.Parameters.AddWithValue("@UserName", us.UserName);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Updated Successfully");
+        }
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                           delete from dbo.Users
+                            where UserId=@UserId
+                        ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("UsersAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@UserId", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Deleted Successfully");
+        }
+    }
+}
---
+++ proiect-back/proiect/modells/Users.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace proiect.modells
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+    }
+}
